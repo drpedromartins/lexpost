13e424accbf3f13a6c8f9085875fa9a32e3d794e
drpedromartins <pedronesmartins@gmail.com>
2026-06-01 23:30:24 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "2mb" }));
 app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "index.html")); });
 app.post("/api/generate", async (req, res) => {
   const apiKey = process.env.ANTHROPIC_API_KEY;
